d9bb96df211f1397d9f81948c2b65126a182bf1d
itsy0gesh <itsyogesh28@gmail.com>
2025-04-28 17:25:23 +0530

    initialised springboot

---
+++ src/test/java/io/mountblue/StackOverflow/StackOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mountblue.StackOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
