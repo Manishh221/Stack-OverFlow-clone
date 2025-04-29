68704b7efe7c8db439a534852ae3069b5e47716e
itsy0gesh <itsyogesh28@gmail.com>
2025-04-29 13:09:50 +0530

    updated UserRepository find by username(email)-login and find by email

--- src/main/java/io/mountblue/StackOverflow/repositories/UserRepository.java
+++ src/main/java/io/mountblue/StackOverflow/repositories/UserRepository.java
@@ -1,17 +1,14 @@
 package io.mountblue.StackOverflow.repositories;
 
 import io.mountblue.StackOverflow.entity.Users;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository  extends JpaRepository<Users, Long>{
+//    to find by email
+    Users findByEmail(String email);
 
-//    find user by email/username.
-    @Query("SELECT u FROM Users u WHERE u.email = :email")
-    Users findByUsername(@Param("email") String email);
-
+//    find user on login
+    Users findByUsername(String username);
 }
