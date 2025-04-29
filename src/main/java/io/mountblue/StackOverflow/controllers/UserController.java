513434127c4913f2835f8c9b3a67ec0cf9f0d840
Ishan Gupta <ishan17052002@gmail.com>
2025-04-29 13:42:10 +0530

    add security dep

--- src/main/java/io/mountblue/StackOverflow/controllers/UserController.java
+++ src/main/java/io/mountblue/StackOverflow/controllers/UserController.java
@@ -1,5 +1,15 @@
 package io.mountblue.StackOverflow.controllers;
 
+import io.mountblue.StackOverflow.entity.Users;
+import io.mountblue.StackOverflow.services.UserService.UserService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class UserController {
     private UserService userService;
     private BCryptPasswordEncoder passwordEncoder;
@@ -19,7 +29,7 @@ public class UserController {
     public String addUser(@ModelAttribute("user") Users user){
         user.setCreatedAt(LocalDateTime.now());
         user.setUpdatedAt(LocalDateTime.now());
-        List<String> splitList = user.getEmail().split('@');
+        List<String> splitList = List.of(user.getEmail().split("@"));
         user.setUserName(splitList.get(0));
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.createNewUser(user);
