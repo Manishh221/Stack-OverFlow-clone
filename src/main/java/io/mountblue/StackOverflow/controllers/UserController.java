16b0d64d6bf49f95f2b881d07cc66870a5629d90
Ishan Gupta <ishan17052002@gmail.com>
2025-04-29 12:23:13 +0530

    added Thymeleaf
    # Conflicts:
    #	.idea/misc.xml

--- src/main/java/io/mountblue/StackOverflow/controllers/UserController.java
+++ src/main/java/io/mountblue/StackOverflow/controllers/UserController.java
@@ -1,6 +1,32 @@
 package io.mountblue.StackOverflow.controllers;
 
 public class UserController {
+    private UserService userService;
+    private BCryptPasswordEncoder passwordEncoder;
 
+    public UserController(UserService userService,BCryptPasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder=passwordEncoder;
+    }
+
+    @GetMapping("/signup")
+    public String showSignup(Model model){
+        model.addAttribute("user",new User());
+        return "Signup";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(@ModelAttribute("user") User user){
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginForm(){
+        return "Login";
+    }
 
 }
