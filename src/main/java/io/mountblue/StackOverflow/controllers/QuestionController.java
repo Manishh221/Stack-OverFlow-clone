a6740c4339cb26975670b2126dc9e043a8bd4448
saikat29 <saikat.pattanaik.32.3@mountblue.tech>
2025-04-29 23:00:42 +0530

    I have added initial code for create questions and home page

--- src/main/java/io/mountblue/StackOverflow/controllers/QuestionController.java
+++ src/main/java/io/mountblue/StackOverflow/controllers/QuestionController.java
@@ -2,6 +2,7 @@ package io.mountblue.StackOverflow.controllers;
 
 import io.mountblue.StackOverflow.entity.Question;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +13,16 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
+    @GetMapping("/ask-question")
+    public String question(Model model) {
+        return "CreateQuestion";
+    }
+
     @PostMapping("/create/question")
     public String createPost(@ModelAttribute("question")Question question) {
 
     return "Home";
     }
+
+
 }
