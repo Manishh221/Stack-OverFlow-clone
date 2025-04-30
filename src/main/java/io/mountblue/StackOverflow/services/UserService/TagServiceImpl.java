342dafc9d024fc72cf06167b3775e84d92bfcce7
Manish <singh.manishh.07@gmail.com>
2025-04-30 11:29:38 +0530

    model attribut added for updatw the question:

--- src/main/java/io/mountblue/StackOverflow/services/UserService/TagServiceImpl.java
+++ src/main/java/io/mountblue/StackOverflow/services/UserService/TagServiceImpl.java
@@ -30,6 +30,7 @@ public class TagServiceImpl implements TagService{
 
     @Override
     public List<Tag> findAllTagsByQuestionId(Long questionId) {
+
        List<QuestionTag> questionTags = questionTagRepository.findByQuestionId(questionId);
        List<Tag> tags = new ArrayList<>();
 
