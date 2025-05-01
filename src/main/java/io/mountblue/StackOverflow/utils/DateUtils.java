21a05357ee70e0d68bc32a2a36c3249ca362936f
Ishan Gupta <ishan17052002@gmail.com>
2025-05-01 11:28:21 +0530

    add dateFormatter

---
+++ src/main/java/io/mountblue/StackOverflow/utils/DateUtils.java
@@ -0,0 +1,15 @@
+package io.mountblue.StackOverflow.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component("dateUtils")
+public class DateUtils {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, hh:mm a");
+
+    public String format(LocalDateTime dateTime) {
+        return dateTime != null ? dateTime.format(formatter) : "";
+    }
+}
