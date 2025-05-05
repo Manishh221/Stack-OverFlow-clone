e40c81486b7429648aa548e9b715319fa010e949
itsy0gesh <itsyogesh28@gmail.com>
2025-05-05 11:26:35 +0530

    addded search tags list and sort in repository

--- src/main/java/io/mountblue/StackOverflow/repositories/TagRepository.java
+++ src/main/java/io/mountblue/StackOverflow/repositories/TagRepository.java
@@ -26,10 +26,15 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
             "ORDER BY COUNT(qt) DESC")
     Page<TagWithCountDTO> searchTagWithQuestionCountSorted(@Param("keyword") String keyword, Pageable pageable);
 
-
     @Query("SELECT new io.mountblue.StackOverflow.dto.TagWithCountDTO(t.tagId, t.tagName, COUNT(qt), t.createdAt) " +
             "FROM Tag t LEFT JOIN QuestionTag qt ON t.tagId = qt.tag.tagId " +
             "GROUP BY t.tagId, t.tagName, t.createdAt " +
             "ORDER BY COUNT(qt) DESC")
+    Page<TagWithCountDTO> findAllTagsSortedByQuestionCount(Pageable pageable);
+
+    @Query("SELECT new io.mountblue.StackOverflow.dto.TagWithCountDTO(t.tagId, t.tagName, COUNT(qt), t.createdAt) " +
+            "FROM Tag t LEFT JOIN QuestionTag qt ON t.tagId = qt.tag.tagId " +
+            "GROUP BY t.tagId, t.tagName, t.createdAt")
     Page<TagWithCountDTO> findAllTagsWithQuestionCount(Pageable pageable);
+
 }
